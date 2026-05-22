3b2e7a21bf748d6c7312cff83d04c2b28af18573
Isnkshwiz <iabdulla082006@gmail.com>
2026-05-23 02:27:09 +0500

    feat(edge): added Edge class

---
+++ src/Edge.java
@@ -0,0 +1,22 @@
+
+
+public class Edge {
+    private final Vertex source;
+    private final Vertex destination;
+    private final int weight;
+
+    public Edge(Vertex source, Vertex destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public Vertex getSource() { return source; }
+    public Vertex getDestination() { return destination; }
+    public int getWeight() { return weight; }
+
+    @Override
+    public String toString() {
+        return source.getId() + " -> " + destination.getId() + " (weight: " + weight + ")";
+    }
+}
